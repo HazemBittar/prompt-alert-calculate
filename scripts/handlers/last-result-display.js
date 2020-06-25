ae9f7464caec9978aa0772db502ee6b8f4f0d4b6
Hazem Bittar <mr.hazem@hotmail.fr>
2020-06-25 12:55:34 +0200

    add function

--- scripts/handlers/last-result-display.js
+++ scripts/handlers/last-result-display.js
@@ -6,6 +6,6 @@ function displayLastResultHandler() {
   console.log('-- action: display lastResult');
   console.log('lastResult:', typeof lastResult, '\n', lastResult);
 
-  alert(`lastResult: ${lastResult}`);
+  alert(`${typeof lastResult}: ${lastResult}`);
 
 }
--- scripts/handlers/last-result-reset.js
+++ scripts/handlers/last-result-reset.js
@@ -5,9 +5,7 @@ console.log('--- loading: reset lastResult');
 function resetLastResultHandler() {
   debugger;
   console.log('\n--- action: reset lastResult ---');
-  console.log('lastResult (before):', typeof lastResult, '\n', lastResult);
-
-  lastResult = 0;
+  
 
   console.log('lastResult (after):', typeof lastResult, '\n', lastResult);
 
